e7650c998e5159a1c7e7a6936923fdf8c3098f08
Ver0-PC <jakub.wrobel27@gmail.com>
2020-01-07 23:33:27 +0100

    Birthday and occupation

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Generator uses tables, written with OOP.
 Generates: 
 -first name 
 -last name 
+-birthday
 -phone number 
 -adress
+-occupation
 
 with name sets:
 -American
--- script.php
+++ script.php
@@ -8,20 +8,27 @@
 		<table>
 		<tr>
 			<th>
-				First Name
+				<i>First Name</i>
 			</th>
 			<th>
-				Last Name
+				<i>Last Name</i>
 			</th>
 			<th>
-				Phone Number
+				<i>Birthday</i>
 			</th>
 			<th>
-				City
+				<i>Phone Number</i>
 			</th>
 			<th>
-				Adress
+				<i>City</i>
 			</th>
+			<th>
+				<i>Adress</i>
+			</th>
+			<th>
+				<i>Occupation</i>
+			</th>
+			
 		</tr>	
 		
 		
@@ -39,19 +46,23 @@
 				if ($nameset == "American") {
 					echo "<th>".$person->getAmericanName()."</th>";
 					echo "<th>".$person->getAmericanLastName()."</th>";
+					echo "<th>".$person->getBirthday()."</th>";
 					echo "<th>".$person->getAmericanNumber()."</th>";
 					$person->getAmericanAdress();
 					echo "<th>".$person->city."</th>";
 					echo "<th>".$person->street.$person->street_num."</th>";
+					echo "<th>".$person->getOccupation()."</th>";
 				}
 
 				if ($nameset == "German") {
 					echo "<th>".$person->getGermanName()."</th>";
 					echo "<th>".$person->getGermanLastName()."</th>";
+					echo "<th>".$person->getBirthday()."</th>";
 					echo "<th>".$person->getGermanNumber()."</th>";
 					$person->getGermanAdress();
 					echo "<th>".$person->city."</th>";
 					echo "<th>".$person->street.$person->street_num."</th>";
+					echo "<th>".$person->getOccupation()."</th>";
 				}
 
 				echo "</tr>";
